86ea80562501419263868e58777602620dc3c4a3
Daniel O'Malley <omalled@lanl.gov>
2017-07-10 16:44:58 -0600

    many changes

--- src/FiniteVolume.jl
+++ src/FiniteVolume.jl
@@ -1,29 +1,31 @@
 module FiniteVolume
 
+import IterativeSolvers
+
 function mydist(x1, x2)
 	return sqrt((x1[1] - x2[1]) ^ 2 + (x1[2] - x2[2]) ^ 2 + (x1[3] - x2[3]) ^ 2)
 end
 
-function solvediffusion(xs, neighbors::Array{Array{Int, 1}, 1}, areas, conductivities, sources, dirichletnodes::Array{Int, 1}, dirichletheads)
+function solvediffusion(volumes::Vector, neighbors::Array{Pair{Int, Int}, 1}, areasoverlengths::Vector, conductivities::Vector, sources::Vector, dirichletnodes::Array{Int, 1}, dirichletheads::Vector)
 	I = Int[]
 	J = Int[]
 	V = Float64[]
-	b = zeros(size(xs, 2))
-	for i = 1:length(dirichletnodes)
+	b = zeros(length(sources))
+	@time for i = 1:length(dirichletnodes)
 		node = dirichletnodes[i]
 		push!(I, node)
 		push!(J, node)
 		push!(V, 1.0)
 		b[node] = dirichletheads[i]
 		if sources[node] != 0
-			error("There is a source at a Dirichlet node")
+			error("There cannot be a source at a Dirichlet node, but node $node is a Dirichlet node where a source is located.")
 		end
 	end
-	freenodes = setdiff(1:size(xs, 2), dirichletnodes)
-	for node1 in freenodes
-		for (i, node2) in enumerate(neighbors[node1])
-			d = norm(xs[:, node1] - xs[:, node2])
-			v = conductivities[node1][i] * areas[node1][i]
+	freenode = fill(true, length(volumes))
+	freenode[dirichletnodes] = false
+	@time for (i, (node1, node2)) in enumerate(neighbors)
+		if freenode[node1]
+			v = conductivities[i] * areasoverlengths[i]
 			push!(I, node1)
 			push!(J, node1)
 			push!(V, v)
@@ -33,8 +35,18 @@ function solvediffusion(xs, neighbors::Array{Array{Int, 1}, 1}, areas, conductiv
 			b[node1] = sources[node1]
 		end
 	end
-	A = sparse(I, J, V)
-	return A \ b
+	@time A = sparse(I, J, V)
+	@time result = A \ b
+	#result = 1
+	#@time result, ch = IterativeSolvers.gmres(A, b; Pl=spdiagm(diag(A)), log=true, maxiter=200, restart=100)
+	#@time result, ch = IterativeSolvers.gmres(A, b; log=true, maxiter=100)
+	#=
+	@show vecnorm(result)
+	@show vecnorm(A * result - b)
+	@show vecnorm(b)
+	@show vecnorm(A * result - b) / vecnorm(b)
+	=#
+	return A, result, ch
 end
 
 end
