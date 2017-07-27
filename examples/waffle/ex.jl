7a5c3407e5f0cb769d96acc8a14b55aeee650edd
Daniel O'Malley <omalled@lanl.gov>
2017-07-27 14:41:57 -0600

    fehm waffle example seems to be working

--- examples/waffle/ex.jl
+++ examples/waffle/ex.jl
@@ -1,3 +1,4 @@
+using Base.Test
 import FEHM
 import FiniteVolume
 import PyPlot
@@ -6,6 +7,7 @@ westzonenum, westnodes = FEHM.parsezone(joinpath(dirname(@__FILE__), "data/out_w
 eastzonenum, eastnodes = FEHM.parsezone(joinpath(dirname(@__FILE__), "data/out_east.zonn"))
 wellzonenums, wellzonenodes = FEHM.parsezone(joinpath(dirname(@__FILE__), "data/well_screens.zonn"))
 isanode, zoneornodenums, skds, eflows, aipeds = FEHM.parseflow(joinpath(dirname(@__FILE__), "data/wl.flow"))
+hycoisanode, hycozoneornodenums, kxs, kys, kzs = FEHM.parsehyco(joinpath(dirname(@__FILE__), "data/w01.hyco"))
 xs, ys, zs, _ = FEHM.parsegeo(joinpath(dirname(@__FILE__), "data/tet.geo"), false)
 r36loc = [5.010628700000E+05, 5.388061300000E+05, 1.765E+03, 1.767E+03, 50]
 r36infilnodes = Int[]
@@ -19,6 +21,7 @@ end
 zonenums = [westzonenum; eastzonenum; wellzonenums; [336000]]
 nodesinzones = [westnodes; eastnodes; wellzonenodes; [r36infilnodes]]
 flownodes, skds, eflows, aipeds = FEHM.flattenzones(zonenums, nodesinzones, isanode, zoneornodenums, skds, eflows, aipeds)
+hyconodes, kxs, kys, kzs = FEHM.flattenzones(zonenums, nodesinzones, hycoisanode, hycozoneornodenums, kxs, kys, kzs)
 dirichletnodes = Int[]
 dirichletheads = Float64[]
 for i = 1:length(flownodes)
@@ -28,9 +31,31 @@ for i = 1:length(flownodes)
 	end
 end
 volumes, areasoverlengths, neighbors = FEHM.parsestor(joinpath(dirname(@__FILE__), "data/tet.stor"))
-A, head, ch = FiniteVolume.solvediffusion(volumes, neighbors, areasoverlengths, ones(length(areasoverlengths)), zeros(length(volumes)), dirichletnodes, dirichletheads)
+hycos = FiniteVolume.fehmhyco2fvhyco(xs, ys, zs, kxs, kys, kzs, neighbors)
+#do the uniform hyco, no recharge case
+@time head, ch, A, b, freenode = FiniteVolume.solvediffusion(volumes, neighbors, areasoverlengths, ones(length(areasoverlengths)), zeros(length(volumes)), dirichletnodes, dirichletheads)
+fehmhead = readdlm("data/w01.00007_sca_node.avs.hom_norecharge"; skipstart=2)[:, 2]
+@show norm(A * head[freenode] - b)
+@show norm(A * fehmhead[freenode] - b)
+@show norm(head - fehmhead)
+@show mean(head - fehmhead)
+@show mean(abs.(head - fehmhead))
 fig, ax = PyPlot.subplots()
 ax[:plot](log10.(ch.data[:resnorm]))
 display(fig); println()
 PyPlot.close(fig)
+
+#do the heterogeneous hyco, no recharge case
+@time hethead, ch, A, b, freenode = FiniteVolume.solvediffusion(volumes, neighbors, areasoverlengths, hycos, zeros(length(volumes)), dirichletnodes, dirichletheads)
+fehmhethead = readdlm("data/w01.00007_sca_node.avs.het_norecharge"; skipstart=2)[:, 2]
+@show norm(A * hethead[freenode] - b)
+@show norm(A * fehmhethead[freenode] - b)
+@show norm(hethead - fehmhethead)
+@show mean(hethead - fehmhethead)
+@show mean(abs.(hethead - fehmhethead))
+fig, ax = PyPlot.subplots()
+ax[:plot](log10.(ch.data[:resnorm]))
+display(fig); println()
+PyPlot.close(fig)
+
 nothing
--- src/FiniteVolume.jl
+++ src/FiniteVolume.jl
@@ -1,7 +1,8 @@
 module FiniteVolume
 
 import IterativeSolvers
-import Preconditioners
+#import Preconditioners
+import PyAMG
 
 function mydist(x1, x2)
 	return sqrt((x1[1] - x2[1]) ^ 2 + (x1[2] - x2[2]) ^ 2 + (x1[3] - x2[3]) ^ 2)
@@ -11,39 +12,75 @@ function solvediffusion(volumes::Vector, neighbors::Array{Pair{Int, Int}, 1}, ar
 	I = Int[]
 	J = Int[]
 	V = Float64[]
-	b = zeros(length(sources))
-	@time for i = 1:length(dirichletnodes)
+	nodei2dirichleti = fill(-1, length(sources))
+	for i = 1:length(dirichletnodes)
 		node = dirichletnodes[i]
-		push!(I, node)
-		push!(J, node)
-		push!(V, 1.0)
-		b[node] = dirichletheads[i]
+		nodei2dirichleti[node] = i
 		if sources[node] != 0
 			error("There cannot be a source at a Dirichlet node, but node $node is a Dirichlet node where a source is located.")
 		end
 	end
-	freenode = fill(true, length(volumes))
+	freenode = fill(true, length(sources))
 	freenode[dirichletnodes] = false
-	@time for (i, (node1, node2)) in enumerate(neighbors)
-		if freenode[node1]
-			v = conductivities[i] * areasoverlengths[i]
-			push!(I, node1)
-			push!(J, node1)
-			push!(V, v)
-			push!(I, node1)
-			push!(J, node2)
-			push!(V, -v)
-			b[node1] = sources[node1]
+	b = Array{Float64}(sum(freenode))
+	nodei2freenodei = fill(-1, length(freenode))
+	j = 1
+	for i = 1:length(freenode)
+		if freenode[i]
+			nodei2freenodei[i] = j
+			b[j] = sources[i]
+			j += 1
 		end
 	end
-	@time A = sparse(I, J, V)
+	for (i, (node1, node2)) in enumerate(neighbors)
+		if freenode[node1] && node1 != node2
+			v = conductivities[i] * areasoverlengths[i]
+			push!(I, nodei2freenodei[node1])
+			push!(J, nodei2freenodei[node1])
+			push!(V, v)
+			if freenode[node2]
+				push!(I, nodei2freenodei[node1])
+				push!(J, nodei2freenodei[node2])
+				push!(V, -v)
+			else
+				b[nodei2freenodei[node1]] += v * dirichletheads[nodei2dirichleti[node2]]
+			end
+		end
+	end
+	A = sparse(I, J, V, sum(freenode), sum(freenode), +)
+	M = PyAMG.aspreconditioner(PyAMG.RugeStubenSolver(A))
+	result, ch = IterativeSolvers.gmres(A, b; Pl=M, log=true, maxiter=400, restart=400, tol=1e-12)
+	#result = PyAMG.solve(PyAMG.RugeStubenSolver(A), b, accel="cg", tol=1e-4)
+	#=
 	@time iL, iU = Preconditioners.ilu0(A)
 	@time result, ch = IterativeSolvers.gmres(A, b; Pl=iL, Pr=iU, log=true, maxiter=400, restart=400)
-	@show vecnorm(result)
-	@show vecnorm(A * result - b)
-	@show vecnorm(b)
-	@show vecnorm(A * result - b) / vecnorm(b)
-	return A, result, ch
+	=#
+	head = Array{Float64}(length(sources))
+	freenodessofar = 0
+	for i = 1:length(sources)
+		if freenode[i]
+			freenodessofar += 1
+			head[i] = result[freenodessofar]
+		else
+			head[i] = dirichletheads[nodei2dirichleti[i]]
+		end
+	end
+	return head, ch, A, b, freenode
+end
+
+function fehmhyco2fvhyco(xs, ys, zs, kxs, kys, kzs, neighbors)
+	ks = Array{eltype(kxs)}(length(neighbors))
+	for i = 1:length(neighbors)
+		node1, node2 = neighbors[i]
+		dx = xs[node1] - xs[node2]
+		dy = ys[node1] - ys[node2]
+		dz = zs[node1] - zs[node2]
+		dist = sqrt(dx^2 + dy^2 + dz^2)
+		k1 = (abs(kxs[node1] * dx) + abs(kys[node1] * dy) + abs(kzs[node1] * dz)) / dist
+		k2 = (abs(kxs[node2] * dx) + abs(kys[node2] * dy) + abs(kzs[node2] * dz)) / dist
+		ks[i] = sqrt(k1 * k2)
+	end
+	return ks
 end
 
 end
