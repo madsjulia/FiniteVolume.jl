6b1e61ec8bc93ced5811fd0d97b686f0db418338
Daniel O'Malley <omalled@lanl.gov>
2017-07-28 09:05:57 -0600

    update waffle example

--- examples/waffle/ex.jl
+++ examples/waffle/ex.jl
@@ -21,22 +21,29 @@ end
 zonenums = [westzonenum; eastzonenum; wellzonenums; [336000]]
 nodesinzones = [westnodes; eastnodes; wellzonenodes; [r36infilnodes]]
 flownodes, skds, eflows, aipeds = FEHM.flattenzones(zonenums, nodesinzones, isanode, zoneornodenums, skds, eflows, aipeds)
-hyconodes, kxs, kys, kzs = FEHM.flattenzones(zonenums, nodesinzones, hycoisanode, hycozoneornodenums, kxs, kys, kzs)
+hyconodes, kxs, kys, kzs = FEHM.flattenzones(zonenums, nodesinzones, hycoisanode, hycozoneornodenums, kxs, kys, kzs)#hyco is in m/s
 dirichletnodes = Int[]
 dirichletheads = Float64[]
+sources = zeros(length(xs))
 for i = 1:length(flownodes)
 	if skds[i] > 0
 		push!(dirichletnodes, flownodes[i])
 		push!(dirichletheads, skds[i])
+	else
+		#this means skds[i] is a mass source in units of kg/s
+		sources[flownodes[i]] = -skds[i] * 1e-3#convert of kg/s to m^3/s
 	end
 end
-volumes, areasoverlengths, neighbors = FEHM.parsestor(joinpath(dirname(@__FILE__), "data/tet.stor"))
+if !isdefined(:volumes)
+	volumes, areasoverlengths, neighbors = FEHM.parsestor(joinpath(dirname(@__FILE__), "data/tet.stor"))
+end
 hycos = FiniteVolume.fehmhyco2fvhyco(xs, ys, zs, kxs, kys, kzs, neighbors)
 #do the uniform hyco, no recharge case
 @time head, ch, A, b, freenode = FiniteVolume.solvediffusion(volumes, neighbors, areasoverlengths, ones(length(areasoverlengths)), zeros(length(volumes)), dirichletnodes, dirichletheads)
 fehmhead = readdlm("data/w01.00007_sca_node.avs.hom_norecharge"; skipstart=2)[:, 2]
 @show norm(A * head[freenode] - b)
 @show norm(A * fehmhead[freenode] - b)
+@show norm(b)
 @show norm(head - fehmhead)
 @show mean(head - fehmhead)
 @show mean(abs.(head - fehmhead))
@@ -50,9 +57,24 @@ PyPlot.close(fig)
 fehmhethead = readdlm("data/w01.00007_sca_node.avs.het_norecharge"; skipstart=2)[:, 2]
 @show norm(A * hethead[freenode] - b)
 @show norm(A * fehmhethead[freenode] - b)
+@show norm(b)
 @show norm(hethead - fehmhethead)
 @show mean(hethead - fehmhethead)
 @show mean(abs.(hethead - fehmhethead))
+fig, ax = PyPlot.subplots()
+ax[:plot](log10.(ch.data[:resnorm]))
+display(fig); println()
+PyPlot.close(fig)
+
+#do the heterogeneous hyco, recharge case
+@time fullhead, ch, A, b, freenode = FiniteVolume.solvediffusion(volumes, neighbors, areasoverlengths, hycos, sources, dirichletnodes, dirichletheads)
+fehmfullhead = readdlm("data/w01.00007_sca_node.avs.het_recharge"; skipstart=2)[:, 2]
+@show norm(A * fullhead[freenode] - b)
+@show norm(A * fehmfullhead[freenode] - b)
+@show norm(b)
+@show norm(fullhead - fehmfullhead)
+@show mean(fullhead - fehmfullhead)
+@show mean(abs.(fullhead - fehmfullhead))
 fig, ax = PyPlot.subplots()
 ax[:plot](log10.(ch.data[:resnorm]))
 display(fig); println()
